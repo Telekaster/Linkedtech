7477ea9cb47e0fe63239385d385e0bf47e2323e9
OLEG <schastliviy.sluchay@ukr.net>
2021-08-18 15:10:48 +0300

    make some stuff

--- js/new_shop.js
+++ js/new_shop.js
@@ -1,15 +1,15 @@
 const activeItem = document.querySelector('.new_shop__item_1');
 const svgList = document.querySelector('.new_shop__svg');
 const priceArea = document.querySelector('.new_shop__price_area');
-console.log(priceArea);
-
+const image = document.querySelector('.new_shop__image_1');
 
 activeItem.addEventListener('click', listClickHandler);
 
 function listClickHandler() {
     svgList.classList.add('js_active');
     priceArea.classList.add('price_active');
-    activeItem.classList.add('list_active')
+    activeItem.classList.add('list_active');
+    image.classList.add('image_active')
 
 }
 
